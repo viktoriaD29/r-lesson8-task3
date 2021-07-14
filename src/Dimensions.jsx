3a5ca043d231b6c33216dbf4ef003fc10ff61ff3
Viktoria <doduchenko29@gmail.com>
2021-07-14 16:39:04 +0300

    add

--- src/Dimensions.jsx
+++ src/Dimensions.jsx
@@ -32,7 +32,7 @@ class Dimensions extends React.Component {
   render() {
     return (
       <div className="dimensions">
-        {`${this.state.width}px - ${this.state.height}`}
+        {`${this.state.width}px - ${this.state.height}px`}
       </div>
     );
   }
